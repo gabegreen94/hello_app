e54498427c15cde1e9ca29db66687cb2bafebe64
Gabe Green <gabegreen94@comcast.net>
2016-08-19 17:59:18 +0000

    application and routes fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def goodbye
+  def hola
     render html: "hola, mundo!"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "application#goodbye"
+  root "application#hola"
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
